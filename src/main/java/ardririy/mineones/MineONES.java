a6957bf50659927c8cdce1f4f6a79b7bedfe07d6
ardRiriy <hynnkgw27@gmail.com>
2021-01-01 04:37:05 +0800

    インゲームにおけるタイマーの実装

--- src/main/java/ardririy/mineones/MineONES.java
+++ src/main/java/ardririy/mineones/MineONES.java
@@ -1,6 +1,11 @@
 package ardririy.mineones;
 
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+
 
 public final class MineONES extends JavaPlugin {
 
--- src/main/java/ardririy/mineones/MineORES_timer.java
+++ src/main/java/ardririy/mineones/MineORES_timer.java
@@ -6,11 +6,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.*;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MineORES_timer implements CommandExecutor{
+    int min = 30;
+    int sec = 0;
+    int all_sec = min*60 + sec;
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         if(cmd.getName().equalsIgnoreCase("timer")){
@@ -34,6 +38,30 @@ public class MineORES_timer implements CommandExecutor{
                 }
             };timer.scheduleAtFixedRate(task, 0, 1000);
         }
+
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+
+        for ( Player player : Bukkit.getOnlinePlayers() ) {
+            player.setScoreboard(board);
+        }
+
+        Objective objective = board.registerNewObjective("time", "dummy");
+        objective.setDisplayName("Information");
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        Timer timerA = new Timer();
+        TimerTask taskA = new TimerTask() {
+            public void run() {
+                Score score = objective.getScore("残り時間：");
+                score.setScore(all_sec);
+                if(all_sec > 0){
+                    all_sec--;
+                }else{
+                    Bukkit.getServer().broadcastMessage("終了！");
+                    timerA.cancel();
+                }
+            }
+        };timerA.scheduleAtFixedRate(taskA,0,1000);
         return false;
     }
 }
