22fb9b49bc0a70e9ad5fa0ef92b021079d5883f3
ardRiriy <hynnkgw27@gmail.com>
2021-01-03 21:33:59 +0800

    ポイントの計測をできるように タイマーの改善

--- src/main/java/ardririy/mineones/MineONES.java
+++ src/main/java/ardririy/mineones/MineONES.java
@@ -1,19 +1,17 @@
 package ardririy.mineones;
 
-
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
 
 
-public final class MineONES extends JavaPlugin {
+
+public class MineONES extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         getCommand("timer").setExecutor(new MineORES_timer());
         getCommand("start").setExecutor(new MineORES_timer());
-
+        getCommand("score").setExecutor(new MineORES_timer());
     }
 
     @Override
